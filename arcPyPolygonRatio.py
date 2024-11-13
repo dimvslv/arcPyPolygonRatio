e491580cdbbcfa5617cba7e2aba0e08269400e6b
Dima <wasilev1994@gmail.com>
2024-11-13 17:50:27 +0500

    2Layers

--- arcPyPolygonRatio.py
+++ arcPyPolygonRatio.py
@@ -4,28 +4,28 @@ import pythonaddins
 mxd = arcpy.mapping.MapDocument("CURRENT")
 layers = arcpy.mapping.ListLayers(mxd)
 
-selected_layer = None
+selected_layers = []  # Список для хранения выбранных слоев
+selected_features = []  # Список для хранения выбранных объектов
 
+# Получаем все выбранные объекты из слоев
 for layer in layers:
     if layer.getSelectionSet():
-        selected_layer = layer
-        break
+        selected_layers.append(layer)
+        # Добавляем выбранные объекты в список
+        selected_features.extend([row for row in arcpy.da.SearchCursor(layer, ["SHAPE@"])])
 
-if layer is None:
-    pythonaddins.MessageBox("Select features to get coordinates", "Info", 0)
-elif int(arcpy.GetCount_management(layer).getOutput(0)) != 2:
-    pythonaddins.MessageBox("Select 2 features", "Info", 0)
+# Проверяем, сколько объектов выбрано
+if len(selected_features) != 2:
+    pythonaddins.MessageBox("Select 2 features in one or two layers", "Info", 0)
 else:
-    selected_features = [row for row in arcpy.da.SearchCursor(layer, ["SHAPE@"])]
-
+    # Получаем площади двух выбранных полигонов
     area1 = selected_features[0][0].getArea("GEODESIC", "SQUAREMETERS")
     area2 = selected_features[1][0].getArea("GEODESIC", "SQUAREMETERS")
 
+    # Рассчитываем процентное соотношение
     if area1 >= area2:
         percent_ratio = (area2 / area1) * 100
         pythonaddins.MessageBox("The smaller polygon is {:.2f}% the larger polygon".format(percent_ratio), "Info", 0)
     else:
         percent_ratio = (area1 / area2) * 100
         pythonaddins.MessageBox("The smaller polygon is {:.2f}% the larger polygon".format(percent_ratio), "Info", 0)
-
-        
