e5ca42c2f5654a8a09d5cc307db0a7215f465c17
Dima <wasilev1994@gmail.com>
2024-11-15 10:13:56 +0500

    launchedInArcGIS

--- README.txt
+++ README.txt
@@ -1,7 +1,7 @@
-v1 
+V0
 Compares the ratio of 2 selected polygons. 
 What percentage does one polygon consist of the other.
 
-v2 
+V1
 Calculates area balance of multiple selected polygons. 
 Shows layer name.
--- arcPyPolygonRatioV1.py
+++ arcPyPolygonRatioV1.py
@@ -10,17 +10,18 @@ selected_features = []
 for layer in layers:
     if layer.getSelectionSet():
         selected_layers.append(layer)
-        selected_features.extend([row for row in arcpy.da.SearchCursor(layer, ["SHAPE@"])])
+        selected_features.extend([(layer.name, row[0]) for row in arcpy.da.SearchCursor(layer, ["SHAPE@"])])
 
-if len(selected_features) != 2:
-    pythonaddins.MessageBox("Select 2 features in one or two layers", "Info", 0)
+if len(selected_features) == 0:
+    pythonaddins.MessageBox("Select polygon features", "Info", 0)
 else:
-    area1 = selected_features[0][0].getArea("GEODESIC", "SQUAREMETERS")
-    area2 = selected_features[1][0].getArea("GEODESIC", "SQUAREMETERS")
+    areas = [(name, feature.getArea("GEODESIC", "HECTARES")) for name, feature in selected_features]
+    total_area = sum(area for _, area in areas)
 
-    if area1 >= area2:
-        percent_ratio = (area2 / area1) * 100
-        pythonaddins.MessageBox("The smaller polygon is {:.2f}% the larger polygon".format(percent_ratio), "Info", 0)
-    else:
-        percent_ratio = (area1 / area2) * 100
-        pythonaddins.MessageBox("The smaller polygon is {:.2f}% the larger polygon".format(percent_ratio), "Info", 0)
+    message = "Total area: {:.1f} ha\n\n".format(total_area)
+    
+    for i, (layer_name, area) in enumerate(areas):
+        percent = (area / total_area) * 100
+        message += "{}: {:.1f} ha ({:.1f}%)\n".format(layer_name, area, percent)
+    
+    pythonaddins.MessageBox(message, "Area Balance", 0)
